25310e76b0f43188fd28440e2cd840b1643bd6e1
anonimatogb <gabrielcavalcante22@outlook.com>
2025-10-23 13:07:01 -0300

    adminmelhor

--- admin.php
+++ admin.php
@@ -2,12 +2,8 @@
 
 require_once "DB/Database.php";
 require_once "Controller/UsuarioController.php";
-require_once "Controller/ProdutoController.php";
-require_once "Controller/PagamentoController.php";
 
 $usuarioController = new UsuarioController($pdo);
-$produtoController = new ProdutoController($pdo);
-$pagamentoController = new PagamentoController($pdo);
 
 $usuarios = $usuarioController->listar();
 
